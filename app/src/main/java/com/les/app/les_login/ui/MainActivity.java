53dd2ef681c2993391821be0d05f37ed7401124b
“esleem” <esleem@solter.co.kr>
2018-04-23 09:27:55 +0900

    구글 로그인 생성

--- app/src/main/java/com/les/app/les_login/ui/MainActivity.java
+++ app/src/main/java/com/les/app/les_login/ui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.les.app.les_login.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -12,15 +14,37 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.les.app.les_login.R;
 import com.les.app.les_login.utils.CommonUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv_userName;
+    private Button btn_logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_main);
+
+        //--------------------
+        // 로그아웃
+        //--------------------
+        btn_logout = (Button) findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //--------------------
+        // 유저 정보 출력
+        //--------------------
+        tv_userName = (TextView) findViewById(R.id.tv_userName);
+        tv_userName.setText("");
 
     }
 }
