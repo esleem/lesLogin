df7d74eb35dd5ed0a1f4e2091cf8e3b9037f8eb4
esleem <esleem@solter.co.kr>
2018-05-14 09:53:45 +0900

    retrolambda 추가 (레트로람다)

--- app/src/main/java/com/les/app/les_login/ui/MainActivity.java
+++ app/src/main/java/com/les/app/les_login/ui/MainActivity.java
@@ -1,12 +1,17 @@
 package com.les.app.les_login.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.les.app.les_login.R;
 import com.les.app.les_login.base.listener.SelectListItemListener;
+import com.les.app.les_login.utils.CommonUtils;
+
+import java.util.Locale;
 
 import static com.les.app.les_login.oauth.SignIn.mSignin;
 
@@ -36,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
     // 기본 셋팅
     //--------------------------------------------------
     private void defaultSetting(){
+
+        //--------------------
+        // 폰번호 유틸
+        //--------------------
+        String oPhone = "01012345678";
+        String phone = PhoneNumberUtils.formatNumber(oPhone, Locale.getDefault().getCountry());
+        CommonUtils.log("@# PHONE : "+ phone);
 
         //--------------------
         // 로그아웃
