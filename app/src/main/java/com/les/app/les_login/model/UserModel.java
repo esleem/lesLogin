fd011090fb32e6291414dfb8e77850872249773c
esleem <esleem@solter.co.kr>
2018-05-15 11:51:30 +0900

    Android data binding 추가

---
+++ app/src/main/java/com/les/app/les_login/model/UserModel.java
@@ -0,0 +1,23 @@
+package com.les.app.les_login.model;
+
+public class UserModel {
+
+    public String name;
+    public String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- app/src/main/java/com/les/app/les_login/ui/MainActivity.java
+++ app/src/main/java/com/les/app/les_login/ui/MainActivity.java
@@ -1,14 +1,15 @@
 package com.les.app.les_login.ui;
 
-import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import com.les.app.les_login.R;
 import com.les.app.les_login.base.listener.SelectListItemListener;
+import com.les.app.les_login.databinding.AcMainBinding;
+import com.les.app.les_login.model.UserModel;
 import com.les.app.les_login.utils.CommonUtils;
 
 import java.util.Locale;
@@ -17,22 +18,25 @@ import static com.les.app.les_login.oauth.SignIn.mSignin;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_userName;
-    private Button btn_logout;
+    private AcMainBinding acMainBinding;
+
+    private UserModel mUserModel = new UserModel();
+    private int mCnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ac_main);
+
+        //--------------------
+        // Rx Android
+        //--------------------
+        acMainBinding = DataBindingUtil.setContentView(this, R.layout.ac_main);
+        acMainBinding.setUser(mUserModel);
 
         //--------------------
         // 기본 셋팅
         //--------------------
         defaultSetting();
-
-        //--------------------
-        // Rx Android
-        //--------------------
 
     }
 
@@ -43,42 +47,75 @@ public class MainActivity extends AppCompatActivity {
     private void defaultSetting(){
 
         //--------------------
-        // 폰번호 유틸
+        // 버튼 - 로그아웃
         //--------------------
-        String oPhone = "01012345678";
-        String phone = PhoneNumberUtils.formatNumber(oPhone, Locale.getDefault().getCountry());
-        CommonUtils.log("@# PHONE : "+ phone);
+        acMainBinding.btnLogout.setOnClickListener(view -> {
 
-        //--------------------
-        // 로그아웃
-        //--------------------
-        btn_logout = (Button) findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            mSignin.logout(new SelectListItemListener() {
+                @Override
+                public void onSelectItem(String position) {
+                    if ("LOGOUT".equals(position)) {
 
-                mSignin.logout(new SelectListItemListener(){
-                    @Override
-                    public void onSelectItem(String position) {
-                        if ("LOGOUT".equals(position)) {
-
-                            finish();
-                        }
+                        finish();
                     }
+                }
 
-                    @Override
-                    public void onSelectItem(int position) {
+                @Override
+                public void onSelectItem(int position) {
 
-                    }
-                });
+                }
+            });
+
+        });
+
+        //--------------------
+        // 버튼 - 체인지
+        //--------------------
+        acMainBinding.btnChange.setOnClickListener(v -> {
+
+            mCnt++;
+
+            String mCntStr = mCnt + "";
+
+            int sum = 0;
+            String[] strings = mCntStr.split("");
+            for (String conso : strings){
+
+                if ("".equals(conso)) conso = "0";
+                int a = Integer.parseInt(conso);
+                sum += a;
             }
+
+            CommonUtils.log("@# sum : " + sum);
+            mCntStr = sum + "";
+            mCntStr = mCntStr.substring(mCntStr.length() - 1, mCntStr.length());
+
+            String notDash = mUserModel.getPhone().replaceAll("-", "");
+            notDash = notDash.substring(0, notDash.length() - 1);
+
+            notDash += mCntStr;
+
+            String nameTarget = mCnt + "name";
+            String phoneTarget = PhoneNumberUtils.formatNumber(notDash, Locale.getDefault().getCountry());
+
+            mUserModel.setPhone(phoneTarget);
+            mUserModel.setName(nameTarget);
+
+            CommonUtils.log("@# n : " + mUserModel.getName() + " / p : " + mUserModel.getPhone());
         });
 
         //--------------------
         // 유저 정보 출력
         //--------------------
-        tv_userName = (TextView) findViewById(R.id.tv_userName);
-        tv_userName.setText(mSignin.getUserName());
+        mUserModel.setName(mSignin.getUserName());
+
+        //--------------------
+        // 폰번호 유틸
+        //--------------------
+        String oPhone = "01012345678";
+        String phone = PhoneNumberUtils.formatNumber(oPhone, Locale.getDefault().getCountry());
+        CommonUtils.log("@# PHONE : "+ phone);
+        mUserModel.setPhone(phone);
 
     }
 
